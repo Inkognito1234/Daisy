e5389def1d22c2e495e4e2504a982ce258b6db36
Wotafak <h.weisenmueller@gmail.com>
2013-04-29 14:27:26 +0200

    Routenspeicherung 1.0

---
+++ Daisy/Route.java
@@ -0,0 +1,42 @@
+
+import lejos.nxt.Motor;
+
+public class Route {
+	// Positionswerte fuer Odometrie,
+	// Orientierung 0 in positiver x-Achsenrichtung
+	protected double positionX;
+	protected double positionY;
+	protected double orientation;
+	// absolute Drehung
+	protected double rotationAbs;
+	
+	// gibt an, ob die Positions- werte aktuell sind oder eventuell
+	// noch eine Methode im Gange ist, die diese gerade Šndert
+	
+	protected boolean positionIsUpdated;
+
+	public Route(Motor left, Motor right, double wheel, double track) {
+		this.resetPos();
+		this.resetRotationAbs();
+	}
+
+	/**
+	 * Setzt die gespeicherte Pose zurueck auf 0,0,0
+	 */
+	protected void resetPos() {
+		positionX = 0;
+		positionY = 0;
+		orientation = 0;
+		// Zuruecksetzen der Motordrehzahlen
+		DriveMotors.rightMotor.resetTachoCount();
+		DriveMotors.leftMotor.resetTachoCount();
+		positionIsUpdated = true;
+	}
+
+	/**
+	 * setzt die absolute gedrehte Orientierung zurueck
+	 */
+	public void resetRotationAbs() {
+		rotationAbs = 0;
+	}
+}
